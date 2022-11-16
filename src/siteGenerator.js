747b8fba61ed4db64cf0b05cf6a7ff084ffb3ec8
OscarVU <oscar.uzdelewicz23@trinityschoolnyc.org>
2022-11-16 10:16:14 -0500

    class update 1

--- src/siteGenerator.js
+++ src/siteGenerator.js
@@ -1,22 +1,22 @@
 const fs = require('fs');
 const ejs = require('ejs');
 
-let character_info = JSON.parse(fs.readFileSync('../data/potter.json', 'utf8'));
+let college_info = JSON.parse(fs.readFileSync('../data/potter.json', 'utf8'));//change name to college file
 let index_template = fs.readFileSync('views/index.ejs', 'utf8');
-let character_template = fs.readFileSync('views/character.ejs', 'utf8');
+let college_template = fs.readFileSync('views/character.ejs', 'utf8');
 
 /*
   1) Generate a web page for each character
   2) Keep track of the link for index.html
 */
-for (character in character_info){
-  let character_html = ejs.render(character_template, {
+for (character in college_info){
+  let character_html = ejs.render(college_template, {
     filename: __dirname + '/views/character.ejs',
-    stats: character_info[character],
+    stats: college_info[character],
     name: character
   });
-  character_info[character].link = getBetterFileName(character);
-  fs.writeFileSync('../public/'+character_info[character].link+'.html', character_html, 'utf8');
+  college_info[character].link = getBetterFileName(character);
+  fs.writeFileSync('../public/'+college_info[character].link+'.html', character_html, 'utf8');
 
 }
 
@@ -25,7 +25,7 @@ for (character in character_info){
 */
 let index_html = ejs.render(index_template, {
   filename: __dirname + '/views/index.ejs',
-  data: character_info
+  data: college_info
 });
 
 fs.writeFileSync('../public/index.html', index_html, 'utf8');
