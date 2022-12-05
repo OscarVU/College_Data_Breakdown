06407b2633dc8d986e135e01ce10a740cd22703c
OscarVU <oscar.uzdelewicz23@trinityschoolnyc.org>
2022-12-05 11:23:46 -0500

    publishing

--- transfer.js
+++ transfer.js
@@ -2,36 +2,36 @@ const fs = require('fs');
 let college_info = JSON.parse(fs.readFileSync('data/colleges.json', 'utf8'));
 
 for(college in college_info){
-  if(fs.existsSync('public/'+college+'.html'))
-    fs.unlinkSync('public/'+college+'.html');
+  if(fs.existsSync('docs/'+college+'.html'))
+    fs.unlinkSync('docs/'+college+'.html');
 }
 
 //manually unlinking all other files
-if(fs.existsSync('public/index.html'))
-fs.unlinkSync('public/index.html');
+if(fs.existsSync('docs/index.html'))
+fs.unlinkSync('docs/index.html');
 
-if(fs.existsSync('public/styles.css'))
-fs.unlinkSync('public/styles.css');
+if(fs.existsSync('docs/styles.css'))
+fs.unlinkSync('docs/styles.css');
 
-if(fs.existsSync('public/about.html'))
-fs.unlinkSync('public/about.html');
+if(fs.existsSync('docs/about.html'))
+fs.unlinkSync('docs/about.html');
 
-if(fs.existsSync('public/macrolofiemmett.jpeg'))
-fs.unlinkSync('public/macrolofiemmett.jpeg');
+if(fs.existsSync('docs/macrolofiemmett.jpeg'))
+fs.unlinkSync('docs/macrolofiemmett.jpeg');
 
-if(fs.existsSync('public/microlofiemmett.jpeg'))
-fs.unlinkSync('public/microlofiemmett.jpeg');
+if(fs.existsSync('docs/microlofiemmett.jpeg'))
+fs.unlinkSync('docs/microlofiemmett.jpeg');
 
-if(fs.existsSync('public/macrolofioscar.jpg'))
-fs.unlinkSync('public/macrolofioscar.jpg');
+if(fs.existsSync('docs/macrolofioscar.jpg'))
+fs.unlinkSync('docs/macrolofioscar.jpg');
 
-if(fs.existsSync('public/microlofioscar.jpg'))
-fs.unlinkSync('public/microlofioscar.jpg');
+if(fs.existsSync('docs/microlofioscar.jpg'))
+fs.unlinkSync('docs/microlofioscar.jpg');
 
 //copying all neccesary files over
-fs.copyFileSync('src/styles.css','public/styles.css');
-//fs.copyFileSync('src/about.html','public/about.html');
-fs.copyFileSync('src/macrolofiemmett.jpeg','public/macrolofiemmett.jpeg');
-fs.copyFileSync('src/microlofiemmett.jpeg','public/microlofiemmett.jpeg');
-fs.copyFileSync('src/macrolofioscar.jpg','public/macrolofioscar.jpg');
-fs.copyFileSync('src/microlofioscar.jpg','public/microlofioscar.jpg');
+fs.copyFileSync('src/styles.css','docs/styles.css');
+//fs.copyFileSync('src/about.html','docs/about.html');
+fs.copyFileSync('src/macrolofiemmett.jpeg','docs/macrolofiemmett.jpeg');
+fs.copyFileSync('src/microlofiemmett.jpeg','docs/microlofiemmett.jpeg');
+fs.copyFileSync('src/macrolofioscar.jpg','docs/macrolofioscar.jpg');
+fs.copyFileSync('src/microlofioscar.jpg','docs/microlofioscar.jpg');
